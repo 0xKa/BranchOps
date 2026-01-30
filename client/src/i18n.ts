41bb7430f441031c5739c08f65fab789b4bb4968
Reda Hilal <reda0x04@gmail.com>
2026-01-31 01:19:40 +0400

    add required lobs and restructure locales

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -2,8 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import arCommon from "./locales/ar.json";
-import enCommon from "./locales/en.json";
+import arCommon from "@/locales/ar/common.json";
+import enCommon from "@/locales/en/common.json";
 
 i18n
   .use(LanguageDetector)
