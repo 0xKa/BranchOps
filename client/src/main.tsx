6c630c8b19c2d322cfbaf8422e8e808d22a77eea
Reda Hilal <reda0x04@gmail.com>
2026-02-01 02:35:14 +0400

    add needed shadcn components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,14 @@
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { DirectionProvider } from "@/components/ui/direction";
+import "@/index.css";
 import { applyLangToHtml, langToDir } from "@/lib/i18n-ui";
+import "@/locales/i18n.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode, useEffect, useMemo } from "react";
 import { createRoot } from "react-dom/client";
 import { useTranslation } from "react-i18next";
 import { RouterProvider } from "react-router";
-import "./i18n";
-import "./index.css";
 import { router } from "./router.tsx";
 
 const queryClient = new QueryClient({
